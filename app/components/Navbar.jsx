c50a0c08bbf077d07536f6f7aa64084018a4585f
uscinaviciusd <uscinaviciusd@gmail.com>
2023-08-23 17:29:41 -0500

    lession 8 checkpoint

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Logo from "./copilot.png";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Image
+        src={Logo}
+        alt="Copilot Logo"
+        width={100}
+        quality={100}
+      />
+      <h1>Menu</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+}
